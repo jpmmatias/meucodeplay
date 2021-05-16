4e066d509714f20963f460df1d8de69c9cef66ff
João Pedro Matias <jpmmatias2018@outlook.com>
2021-05-16 13:13:09 -0300

    Adicionando testes para resgitrar cursos e professores

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
-RSpec.describe Teacher, type: :model do
-	pending "add some examples to (or delete) #{__FILE__}"
+describe Teacher do
+	context 'validation' do
+		it 'attributes cannot be blank' do
+			teacher = Teacher.new
+
+			teacher.valid?
+
+			expect(teacher.errors[:name]).to include('não pode ficar em branco')
+			expect(teacher.errors[:email]).to include('não pode ficar em branco')
+		end
+
+		it 'email must be uniq' do
+			Teacher.create!(
+				name: 'Henrique',
+				bio: 'Professor de Ruby',
+				email: 'henrique@gmail.com',
+			)
+			teacher = Teacher.new(email: 'henrique@gmail.com')
+
+			teacher.valid?
+
+			expect(teacher.errors[:email]).to include('email já está em uso')
+		end
+	end
 end
